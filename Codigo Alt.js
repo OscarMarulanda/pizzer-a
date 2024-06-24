e05199273384b3f71215e4fe0d2bfa41a2c23042
Oscar <odmarulandab@unal.edu.co>
2024-06-24 18:05:19 -0500

    change name to index

--- Codigo Alt.js
+++
@@ -1,8 +0,0 @@
-function generateCode() {
-            return Math.floor(1000 + Math.random() * 9000);
-        }
-
-        window.onload = function() {
-            const codeElement = document.getElementById('codigo');
-            codeElement.textContent = generateCode();
-        };
